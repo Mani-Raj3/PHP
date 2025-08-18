af32eade4456d6447f24ac238e0f484d9cad3b92
Mani-Raj3 <maniraj31102@gmail.com>
2025-08-18 23:21:01 +0530

    Add files via upload

---
+++ First.php
@@ -0,0 +1,17 @@
+<html><head><title>This is a file of PHP Program</title></head>
+<body>
+    <center>
+        <marquee>This is my First php Code</marquee>
+<!-- <?php
+echo "Mani Raj";
+print("<br> This is my PHP Certification");
+?> -->
+
+<?php
+  $a = 100;
+  $b = 50;
+  $c =$a + $b;
+  echo "Addition of".$a." and" .$b. "is". $c;
+?>
+</center>
+</body>
