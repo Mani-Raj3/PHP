eb982c41bca7e7bca83e2872479b35be16064f05
Mani-Raj3 <maniraj31102@gmail.com>
2025-08-27 02:07:40 +0530

    Add files via upload

---
+++ Constant.php
@@ -0,0 +1,13 @@
+ <html><head><title>Data Type in PHP Program</title></head>
+<body>
+    <center>
+<?php
+ $a =10;
+ $b = 20;
+ $c = $a + $b;
+ echo "Addition of " .$a. "and" .$b. "is" .$c;
+
+?>
+
+    </center>
+</body>
